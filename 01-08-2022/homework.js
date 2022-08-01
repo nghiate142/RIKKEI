1b8d13787f54d3617ca5778d71ea2df35326777d
Vũ Hoàng Đức <duc10a2lc97@gmail.com>
2022-08-01 22:56:04 +0700

    homework.js

--- 01-08-2022/homework.js
+++ 01-08-2022/homework.js
@@ -20,3 +20,98 @@
         //     a = b
         //     b = c
         // }
+
+
+
+
+
+// EX3: Tìm số đầu tiên trong dãy fibonacci chia hết cho 5.
+    //code 
+        // let a = 0
+        // let b = 1 
+        // let c 
+        // let d 
+        // for (i = 1; i < 20; i += 1) {
+        //     console.log(a);  
+        //     // if (a % 5 === 0) break;
+                 
+        //         c = a + b;
+        //         a = b
+        //         b = c
+        //         if (a > 1 && a % 5 === 0){
+        //             console.log(a, ' là số chia hết cho 5');
+        //             break                 
+        //         }           
+        // }
+
+
+
+
+// EX4: Tính tổng của 20 số đầu tiên trong dãy fibonacci.
+    //code
+    // let a = 0
+    // let b = 1 
+    // let c 
+    // let plus = 0
+    // for (i = 1; i <= 20; i += 1) {
+    //     // console.log(a)
+    //     c = a + b;
+    //     a = b; 
+    //     b = c;
+    //     plus = plus + a;
+    //     // console.log(plus)
+    // }
+    // console.log(plus)
+
+
+
+
+
+// EX5: Tính tổng của 30 số chia hết cho 7 đầu tiên trong các số tự nhiên.
+    //code
+        // let plus
+        // for (i = 0; i <= 30; i += 1){
+        //     if (i % 7 === 0){
+        //         // console.log(i)
+        //     plus = ((1 + i) * 30) * 7 / 2;
+        //     // console.log(plus)
+        //     }
+        // }
+        // console.log(plus)
+
+  
+
+
+
+
+// EX6: Hãy viết một chương trình in ra các số từ 1 đến 100. Nhưng nếu số chia hết cho 3 thì in ra "Fizz", 5 thì in ra "Buzz" thay vì in ra số đó
+// Và nếu số đó chia hết cho cả 3 và 5 thì in ra chữ "FizzBuzz".
+    //code
+        // for (i = 1; i <=100; i += 1) {
+        //     console.log(i)
+        //     if (i % 3 === 0 && i % 5 !=0){
+        //         console.log(' Fizz');
+        //     }
+        //     if (i % 5 === 0 && i % 3 !=0){
+        //         console.log(' Buzz')
+        //     }
+        //     if (i % 3 === 0 && i % 5 ===0){
+        //         console.log(' FizzBuzz')
+        //     }
+        // }
+            
+
+
+
+
+
+// EX7: Tính giai thừa của một số nguyên dương
+let factorial = parseInt(prompt('Điền một số bất kỳ'));
+    result = 1
+    for (i = 1; i <= factorial; i += 1){
+        result = result * i;
+        // console.log(result)
+    }
+    console.log(result)
+
+    
